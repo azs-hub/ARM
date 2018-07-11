9ab20815eac53a7fba2566ad2ed2aedf0aabdbf0
Anais Siba <anais.siba@gmail.com>
2018-07-11 10:21:12 +0530

    [Header] - Init navigation

--- gulpfile.js
+++ gulpfile.js
@@ -59,8 +59,6 @@ gulp.task("watch", ["fileinclude", "browserSync", "sass", 'es6'], function () {
     gulp.watch($app + "/public/scss/*.scss", ["sass", browserSync.reload]);
     gulp.watch($app + "/*.html", ["fileinclude"]);
     gulp.watch($app + "/public/js/**/*.js", ['es6']);
-    // gulp.watch($app + "/public/js/**/*.ts", ["ts"]);
-    // gulp.watch($app + "/public/partials/*.html", ["fileinclude"]);
     gulp.watch($dist + "/*.html", browserSync.reload);// on ne recharge que les preview
     gulp.watch($dist + "/css/**/*.css", browserSync.reload);
     gulp.watch($dist + "/js/**/*.js", browserSync.reload);
@@ -72,44 +70,20 @@ gulp.task("useref", function () {
         .pipe(gulp.dest($dist));
 });
 
-gulp.task("fonts", function () {
-    return gulp.src($app + "/public/fonts/**/*")
-        .pipe(cache(imagemin({
-            interlaced: true
-        })))
-        .pipe(gulp.dest($dist + "/fonts"));
-});
-
 gulp.task("img", function () {
     return gulp.src($app + "/public/images/**/*.+(png|jpg|jpeg|gif|svg)")
         .pipe(gulp.dest($dist + "/images"));
 });
-
-/*gulp.task("svg", function () {
-    return gulp.src($app + "/public/svg/*.svg")
-        .pipe(gulp.dest($dist + "/svg"));
-});*/
-
 gulp.task("js", function () {
     return gulp.src($app + "/public/js/**/*.js")
         .pipe(gulp.dest($dist + "/js"));
 });
-
-// gulp.task('es6', function(){
-//     gulp.src($app + "/public/js/*.js")
-//         .pipe(babel({"presets": ["es2015"]}))
-//         .pipe(gulp.dest($dist + "/js"))
-// });
-
 gulp.task('es6', function(){
     // set up the browserify instance on a task basis
     var b = browserify({
         entries: [
-            "app/public/js/AnimGsap.js",
-            "app/public/js/ArrowAnim.js",
-            "app/public/js/LogoAnim.js",
-            "app/public/js/WelcomeAnim.js",
-            "app/public/js/main.js"],
+            //"app/public/js/main.js"
+        ],
         debug: true,
         // defining transforms here will avoid crashing your stream
         transform: [babelify.configure({
@@ -129,20 +103,13 @@ gulp.task('es6', function(){
 });
 
 gulp.task("css", function () {
-    return gulp.src($app + "/public/css/*.css")
+    return gulp.src($app + "/public/css/**/*.css")
         .pipe(gulp.dest($dist + "/css"));
 });
 
 gulp.task("clean:dist", function (callback) {
     return del([$dist + "/**/*", "!" + $dist + "/img", "!" + $app + "/img/**/*"], callback);
 });
-
-gulp.task('replace-html-src', function () {
-    return gulp.src([$app + "/index.html"])
-        .pipe(replace('/prod/', ''))
-        .pipe(gulp.dest($dist));
-});
-
 gulp.task("clean", function (callback) {
     del([$dist]);
     return cache.clearAll(callback);
@@ -157,17 +124,6 @@ gulp.task("fileinclude", function () {
         .pipe(gulp.dest("prod/"));
 });
 
-
-// gulp.task('symlink-matterJs', function () {
-//     return gulp.src('./node_modules/matter-js')
-//         .pipe(symlink("./app/prod/js/libs/matter-js"));
-// });
-
-// gulp.task('symlink-requireJS', function () {
-//     return gulp.src('./node_modules/requirejs')
-//         .pipe(symlink("./app/prod/js/libs/requirejs"));
-// });
-
 gulp.task("default", function (callback) {
-    runSequence(["sass", "img", "fonts", "js", 'es6', "css", "fileinclude", "browserSync", "watch"], callback);
+    runSequence(["sass", "img", "js", 'es6', "css", "fileinclude", "browserSync", "watch"], callback);
 });
